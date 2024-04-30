5616ec2924ce7108fc5d2be1c4bc4556cf465609
DusterTheFirst <DusterTheFirst@users.noreply.github.com>
2024-04-30 19:36:51 +0000

    Deploy preview for PR 85 🛫

--- preview/pr-85/admin/template.js
+++ preview/pr-85/admin/template.js
@@ -1,4 +1,8 @@
-CMS.registerPreviewTemplate("teams", ({ entry, widgetFor, widgetsFor }) => {
+// CMS.registerPreviewStyle("preview.css");
+
+CMS.registerPreviewTemplate("teams", ({ widgetFor, widgetsFor, getAsset }) => {
+  const members_widgets = widgetsFor("members").toJS();
+
   return h(
     "div",
     {},
@@ -6,20 +10,39 @@ CMS.registerPreviewTemplate("teams", ({ entry, widgetFor, widgetsFor }) => {
     widgetFor("description"),
     h(
       "div",
-      {},
-      widgetsFor("members").map((member, index) => {
-        return h(
-          "div",
-          { key: index /*,  style: { display: "flex", flexDirection: "row" } */ },
-          h("pre", {}, `${member.getIn(["data", "first_name"])} ${member.getIn(["data", "last_name"])}`),
-          h("pre", {}, member.getIn(["data", "function"])),
-          h("pre", {}, member.getIn(["data", "subteams"]).join(", ")),
-          h("pre", {}, member.getIn(["data", "email"])),
-          h("div", { style: { maxHeight: "10em" } }, member.getIn(["widgets", "photo"])),
-          h("pre", {}, JSON.stringify(member))
-        );
-      })
-    ),
-    h("pre", {}, JSON.stringify(entry.getIn(["data", "members"]), undefined, 4))
+      {
+        style: {
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, 8em minmax(30ch, 1fr))",
+          gridTemplateRows: "10em",
+          gap: "1em"
+        }
+      },
+      Array.isArray(members_widgets) &&
+        widgetsFor("members").map((member, index) => {
+          return [
+            h("img", {
+              key: index,
+              style: { height: "100%", width: "100%", objectFit: "contain" },
+              src: getAsset(member.getIn(["data", "photo"])).toString() // TODO: Placeholder image
+            }),
+            h(
+              "div",
+              { style: { marginBlock: "auto" }, key: index },
+              `${member.getIn(["data", "first_name"]) ?? ""} ${member.getIn(["data", "last_name"]) ?? ""}`,
+              h("br"),
+              member.getIn(["data", "function"]) ?? "",
+              h("br"),
+              member.getIn(["data", "study"]) ?? "",
+              h("br"),
+              (member.getIn(["data", "subteams"]) ?? []).join(" + "),
+              h("br"),
+              member.getIn(["data", "email"]) ?? "",
+              h("br"),
+              member.getIn(["data", "linkedin"]) ?? ""
+            )
+          ];
+        })
+    )
   );
 });
